6f1fc0d89129992f7dced30db6c591fe4d912bc5
Aliyo254 <alinurali254@gmail.com>
2022-05-30 14:56:28 +0300

    adconnected database

--- galleryapp/settings.py
+++ galleryapp/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'galleryapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'alinur',
+    'PASSWORD':'admin',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
