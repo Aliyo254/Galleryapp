4af1f1a99c79bba75ea19d32539008e361b73361
Aliyo254 <alinurali254@gmail.com>
2022-05-31 16:01:06 +0300

    Established foreign keys

--- photos/models.py
+++ photos/models.py
@@ -2,20 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Image(models.Model):
-    image = models.ImageField(blank=False)
-    name = models.CharField(max_length=60)
-    description = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.image
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.delete()
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
@@ -39,4 +26,22 @@ class Location(models.Model):
         self.save()
 
     def delete_location(self):
+        self.delete()
+
+class Image(models.Model):
+    image = models.ImageField(blank=False)
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+
+    def __str__(self):
+        return self.image
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
         self.delete()
