a339effab03ffab62a1b7713c79571992afbe6b4
Aliyo254 <alinurali254@gmail.com>
2022-06-02 12:50:38 +0300

    Settings for static files set

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from .models import Image
 
-# Create your views here.
-from django.shortcuts import render
-from django.http  import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'photos/index.html')
+    images = Image.objects.all()
+    print (images)
+    return render(request, 'photos/index.html', {'images': images})
